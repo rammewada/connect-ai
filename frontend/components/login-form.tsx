7350707acec4ea774af9a3b73d8dd316c675a0cc
rammewada <sitarammewada077@gmail.com>
2026-06-11 23:58:30 +0530

    Add login with google account

--- frontend/components/login-form.tsx
+++ frontend/components/login-form.tsx
@@ -1,61 +1,102 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
+import { useState } from "react";
 
-import { cn } from '@/lib/utils'
-import { createClient } from '@/lib/client'
-import { Button } from '@/components/ui/button'
+import { cn } from "@/lib/utils";
+import { createClient } from "@/lib/client";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card'
+} from "@/components/ui/card";
 
-export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
-  const [error, setError] = useState<string | null>(null)
-  const [isLoading, setIsLoading] = useState(false)
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  const [error, setError] = useState<string | null>(null);
+  const [isLoadingGithub, setIsLoadingGithub] = useState(false);
+  const [isLoadingGoogle, setIsLoadingGoogle] = useState(false);
 
-  const handleSocialLogin = async (e: React.FormEvent) => {
-    e.preventDefault()
-    const supabase = createClient()
-    setIsLoading(true)
-    setError(null)
+  const handleSocialLoginWithGithub = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const supabase = createClient();
+    setIsLoadingGithub(true);
+    setError(null);
 
     try {
       const { error } = await supabase.auth.signInWithOAuth({
-        provider: 'github',
+        provider: "github",
         options: {
           redirectTo: `${window.location.origin}/auth/oauth?next=/protected`,
         },
-      })
+      });
 
-      if (error) throw error
+      if (error) throw error;
     } catch (error: unknown) {
-      setError(error instanceof Error ? error.message : 'An error occurred')
-      setIsLoading(false)
+      setError(error instanceof Error ? error.message : "An error occurred");
+      setIsLoadingGithub(false);
     }
-  }
+  };
+
+  const handleSocialLoginWithGoogle = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const supabase = createClient();
+    setIsLoadingGoogle(true);
+    setError(null);
+
+    try {
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider: "google",
+        options: {
+          redirectTo: `${window.location.origin}/auth/oauth?next=/protected`,
+        },
+      });
+
+      if (error) throw error;
+    } catch (error: unknown) {
+      setError(error instanceof Error ? error.message : "An error occurred");
+      setIsLoadingGoogle(false);
+    }
+  };
 
   return (
-    <div className={cn('flex flex-col gap-6', className)} {...props}>
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
           <CardTitle className="text-2xl">Welcome!</CardTitle>
           <CardDescription>Sign in to your account to continue</CardDescription>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleSocialLogin}>
+          <form onSubmit={handleSocialLoginWithGithub}>
             <div className="flex flex-col gap-6">
               {error && <p className="text-sm text-destructive-500">{error}</p>}
-              <Button type="submit" className="w-full" disabled={isLoading}>
-                {isLoading ? 'Logging in...' : 'Continue with GitHub'}
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={isLoadingGithub}
+              >
+                {isLoadingGithub ? "Logging in..." : "Continue with GitHub"}
+              </Button>
+            </div>
+          </form>
+          <form onSubmit={handleSocialLoginWithGoogle}>
+            <div className="flex flex-col gap-6">
+              {error && <p className="text-sm text-destructive-500">{error}</p>}
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={isLoadingGoogle}
+              >
+                {isLoadingGoogle ? "Logging in..." : "Continue with Google"}
               </Button>
             </div>
           </form>
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
